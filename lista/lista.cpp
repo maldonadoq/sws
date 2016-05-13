37a3cc48e4a5023b2f5ba201a115d8bdb853227d
percy00010 <percy.maldonado@ucsp.edu.pe>
2016-05-13 08:43:07 -0500

    asd

---
+++ lista/lista.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+class nodo
+{
+    public:
+        int _dato;
+        nodo *sig;
+        nodo(int );
+};
+
+class lista
+{
+    public:
+        lista();
+        nodo *primero;
+        nodo *ultimo;
+        void inser(int );
+        void print();
+};
+
+nodo::nodo(int a)
+{
+    _dato=a;
+    sig=NULL;
+}
+
+
+lista::lista()
+{
+    primero=NULL;
+    ultimo=NULL;
+}
+
+void lista::inser(int x)
+{
+    if(!primero){
+        primero = new nodo(x);
+        ultimo = primero;
+    }
+    else{
+        ultimo->sig = new nodo(x);
+        ultimo = ultimo->sig;
+    }
+}
+
+void lista:: print()
+{
+    nodo* temp;
+    temp=primero;
+    while(temp != ultimo->sig){
+        cout << temp->_dato << endl;
+        temp = ultimo->sig;
+    }
+}
+
+
+int main()
+{
+    lista sws;
+    sws.inser(4);
+    sws.print();
+    lista w;
+    w.inser(3);
+    w.print();
+    return 0;
+}
+
